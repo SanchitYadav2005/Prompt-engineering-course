d150e2a97920d6894c09cc23503923ab2e17855d
unknown <sanchit2005yudavanshi@gmail.com>
2023-10-15 18:28:44 +0530

    making a transcript app

--- app.py
+++ app.py
@@ -1,17 +1,5 @@
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
 import openai
-import os
-
-
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
-
-openai.api_key  = os.getenv('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')
-
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0, # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
+openai.api_key = "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"
+audio_file= open("song.mp3", "rb")
+transcript = openai.Audio.transcribe("whisper-1", audio_file)
